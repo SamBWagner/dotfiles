a0024d7b795789c437086fde29a2b45d960deddf
Sam Wagner <samuel.b.wagner@gmail.com>
2025-07-24 08:22:38 +1000

    change something

--- nvim/lua/SamBWagner/set.lua
+++ nvim/lua/SamBWagner/set.lua
@@ -1,26 +1,16 @@
-vim.opt.nu = true
-vim.opt.relativenumber = true
+vim.opt.nu = true                 -- set line numbers -- set line numbers
+vim.opt.relativenumber = true     -- use relative line numbers
+vim.opt.scrolloff = 10
 
+-- set tab size to 4 spaces
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
-
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
 
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
+vim.opt.incsearch = true -- incremental search
 
 vim.opt.termguicolors = true
-
-vim.opt.scrolloff = 10
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
-
-vim.opt.updatetime = 50
-
-vim.opt.colorcolumn = "80"
-
-vim.g.mapleader = " "
