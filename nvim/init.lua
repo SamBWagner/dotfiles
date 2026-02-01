e171e9c28f66f40f4a5c8069f4c9a5286d659948
Sam Wagner <sam.wagner@hey.com>
2026-02-01 19:45:43 +1000

    add markdown file handling

--- nvim/init.lua
+++ nvim/init.lua
@@ -44,6 +44,61 @@ vim.api.nvim_create_autocmd("FileType", {
     end,
 })
 
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "markdown",
+    callback = function(event)
+        vim.opt_local.wrap = true
+        vim.opt_local.linebreak = true
+        vim.opt_local.spell = true
+        vim.opt_local.spelllang = { "en_us" }
+        vim.opt_local.colorcolumn = ""
+        vim.opt_local.conceallevel = 2
+
+        local function show_spell_suggestions()
+            local word = vim.fn.spellbadword()[1]
+            if not word or word == "" then
+                return false
+            end
+
+            local suggestions = vim.fn.spellsuggest(word, 8)
+            local message
+            if #suggestions == 0 then
+                message = string.format("No suggestions for \"%s\"", word)
+            else
+                message = string.format(
+                    "Suggestions for \"%s\": %s",
+                    word,
+                    table.concat(suggestions, ", ")
+                )
+            end
+
+            vim.notify(message, vim.log.levels.INFO, { title = "Spelling" })
+            return true
+        end
+
+        local function hover_or_spell()
+            if not show_spell_suggestions() then
+                vim.lsp.buf.hover({ border = "rounded" })
+            end
+        end
+
+        local function diagnostics_or_spell()
+            if not show_spell_suggestions() then
+                vim.diagnostic.open_float(nil, { border = "rounded" })
+            end
+        end
+
+        vim.keymap.set("n", "K", hover_or_spell, { buffer = event.buf, desc = "Hover or spelling suggestions" })
+        vim.keymap.set("n", "<C-k>", hover_or_spell, { buffer = event.buf, desc = "Hover or spelling suggestions" })
+        vim.keymap.set(
+            "n",
+            "gdi",
+            diagnostics_or_spell,
+            { buffer = event.buf, desc = "Diagnostics or spelling suggestions" }
+        )
+    end,
+})
+
 vim.pack.add({
     { src = "https://github.com/sainnhe/sonokai" },
     { src = "https://github.com/akinsho/toggleterm.nvim" },
